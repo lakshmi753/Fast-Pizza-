423215ac30dd880252ae234a6331d5556bd9c21e
lakshmi753 <lakshmi753sahu@gmail.com>
2024-07-31 16:16:34 +0530

    A new component 'UserName' is added and some components like 'Home and Header' are styled little bit using tailwind css .

--- fast_pizza_co/src/ui/Header.jsx
+++ fast_pizza_co/src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast Pizza Co.</Link>
+    <header className="bg-yellow-400">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Jonas</p>
+      <UserName />
     </header>
   );
 }
