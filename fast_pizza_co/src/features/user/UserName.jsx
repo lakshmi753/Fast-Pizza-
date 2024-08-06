a4f45a47046279fce11554f69add9e5aa782cd42
lakshmi753 <lakshmi753sahu@gmail.com>
2024-08-06 13:27:42 +0530

    user state is implemented in all the required components.

--- fast_pizza_co/src/features/user/UserName.jsx
+++ fast_pizza_co/src/features/user/UserName.jsx
@@ -1,13 +1,11 @@
 import { useSelector } from 'react-redux';
 
 function UserName() {
-  const userName = useSelector((store) => store.user.username);
+  const username = useSelector((store) => store.user.username);
 
-  if (!userName) return;
+  if (!username) return;
 
-  return (
-    <div className="hidden text-sm font-semibold md:block">{userName}</div>
-  );
+  return <div className="text-sm font-semibold md:block">{username}</div>;
 }
 
 export default UserName;
