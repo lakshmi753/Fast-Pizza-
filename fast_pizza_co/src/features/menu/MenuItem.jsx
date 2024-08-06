cce40e7529726edb10401975d5e1e30163ff0dae
lakshmi753 <lakshmi753sahu@gmail.com>
2024-08-06 17:18:25 +0530

    Functionality of adding new item to the cart is implemented.

--- fast_pizza_co/src/features/menu/MenuItem.jsx
+++ fast_pizza_co/src/features/menu/MenuItem.jsx
@@ -1,8 +1,26 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utilities/helpers';
+import { addItems } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    //console.log(id);
+
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItems(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +43,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
