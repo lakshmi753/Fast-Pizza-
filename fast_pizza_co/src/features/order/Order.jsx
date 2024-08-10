09a71b49fcecae97d2700964a906c1dc1a409c47
lakshmi753 <lakshmi753sahu@gmail.com>
2024-08-10 20:43:22 +0530

    Adding the feature of making an order priority even after submitting the order.

--- fast_pizza_co/src/features/order/Order.jsx
+++ fast_pizza_co/src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../utilities/helpers';
 import OrderItem from './OrderItem';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const orderData = useLoaderData();
@@ -91,6 +92,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={orderData} />}
     </div>
   );
 }
